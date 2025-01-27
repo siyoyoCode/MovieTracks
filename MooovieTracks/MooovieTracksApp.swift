71363b98a49e1e55f128195a68e2a64bf6b3d447
siyoyoCode <siyona101215@gmail.com>
2025-01-26 17:02:58 -0800

    Initial Commit

---
+++ MooovieTracks/MooovieTracksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MooovieTracksApp.swift
+//  MooovieTracks
+//
+//  Created by Siyona Dhingra on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MooovieTracksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MooovieTracksTests/MooovieTracksTests.swift
@@ -0,0 +1,17 @@
+//
+//  MooovieTracksTests.swift
+//  MooovieTracksTests
+//
+//  Created by Siyona Dhingra on 1/26/25.
+//
+
+import Testing
+@testable import MooovieTracks
+
+struct MooovieTracksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

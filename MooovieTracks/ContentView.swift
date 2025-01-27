62116539b31e47f97eec1ed18a2607687a430f97
siyoyoCode <siyona101215@gmail.com>
2025-01-26 17:33:11 -0800

    please let git work

--- MooovieTracks/ContentView.swift
+++ MooovieTracks/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Please let git work")
         }
         .padding()
     }
